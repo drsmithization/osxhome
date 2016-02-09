b042aa4f68b079c74308edda2dd7a5ecbfb228a9
Dmitry <dkuznetsov@yandex.ru>
2016-02-09 23:17:39 +0100

    commands to install gcc5, python, pip and virtualenv using MacPorts

---
+++ ports.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -ex
+
+sudo port selfupdate
+sudo port install gcc5
+sudo port select --set gcc gcc5
+hash -r
+sudo port install g95
+
+sudo port install python27
+sudo port select --set python python27
+sudo port install py27-pip
+sudo port select --set pip pip27
+sudo port install py27-virtualenv
+sudo port select --set virtualenv virtualenv27
